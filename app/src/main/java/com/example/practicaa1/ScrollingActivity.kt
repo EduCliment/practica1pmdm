0ca172266f689b906c9166670f09b6d2e741bda2
EduCliment <eduardoccp2003@gmail.com>
2022-10-05 18:41:41 +0200

    Practica 1

--- app/src/main/java/com/example/practicaa1/ScrollingActivity.kt
+++ app/src/main/java/com/example/practicaa1/ScrollingActivity.kt
@@ -25,9 +25,14 @@ class ScrollingActivity : AppCompatActivity() {
         binding = ActivityScrollingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val link1 = Intent(Intent.ACTION_VIEW, Uri.parse("https://aules.edu.gva.es/fp/my/index.php"))
+        val link1 = Intent(Intent.ACTION_VIEW, Uri.parse("https://es.wikipedia.org/wiki/Cristiano_Ronaldo"))
+        val link2 = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.realmadrid.com/landings/copas-de-europa-ganadas/index.html"))
+        val link3 = Intent(Intent.ACTION_VIEW, Uri.parse("https://es.wikipedia.org/wiki/Copa_del_Rey_de_f%C3%BAtbol_2021-22")) // JEJEJEJE LA PIERDE EL VALENCIA NOBATOS
+        // JEJEJEJE LA PIERDE EL VALENCIA NOBATOS
 
-
+        binding.content.btnBuy.setOnClickListener{startActivity(link1)}
+        binding.content.btnBuy2.setOnClickListener{startActivity(link2)}
+        binding.content.btnBuy3.setOnClickListener{startActivity(link3)}
 
         loadImage()
         loadImage2()
@@ -42,7 +47,7 @@ class ScrollingActivity : AppCompatActivity() {
         return true
     }
 
-    private fun loadImage(url: String = "https://img.vavel.com/fb32yeawyaaypa8-1-1662376801173.jpg") {
+    private fun loadImage(url: String = "https://e00-expansion.uecdn.es/assets/multimedia/imagenes/2021/06/10/16233199183125.jpg") {
         Glide.with(this)
             .load(url)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
@@ -50,14 +55,14 @@ class ScrollingActivity : AppCompatActivity() {
             .into(binding.content.imgCard)
     }
 
-    private fun loadImage2(url: String = "https://img.vavel.com/fb32yeawyaaypa8-1-1662376801173.jpg") {
+    private fun loadImage2(url: String = "https://www.larazon.es/resizer/X2BVfs5s0pKmmodTqnyTD5lC4Fg=/600x400/smart/filters:format(webp):quality(65)/cloudfront-eu-central-1.images.arcpublishing.com/larazon/BTIILBNG5FAO7DQH6ZAUN4VYO4.PNG") {
         Glide.with(this)
             .load(url)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .centerCrop()
             .into(binding.content.imgCard2)
     }
-    private fun loadImage3(url: String = "https://img.vavel.com/fb32yeawyaaypa8-1-1662376801173.jpg") {
+    private fun loadImage3(url: String = "https://www.futbolred.com/files/article_main/uploads/2018/02/09/5a7d9b15b304c.jpeg") {
         Glide.with(this)
             .load(url)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
